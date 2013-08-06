ffe5aefc3697c00e23db066304d3e3d7543498ae
lroth <lukasz.roth@gmail.com>
2013-08-06 17:03:42 +0200

    services

--- Service/NewsContentProvider.php
+++ Service/NewsContentProvider.php
@@ -2,7 +2,6 @@
 
 namespace Btn\NewsBundle\Service;
 
-use Symfony\Component\HttpFoundation\Request;
 use Btn\NodesBundle\Service\NodeContentProviderInterface;
 use Btn\NewsBundle\Form\NodeContentType;
 
@@ -12,15 +11,31 @@ use Btn\NewsBundle\Form\NodeContentType;
 */
 class NewsContentProvider implements NodeContentProviderInterface
 {
+
+    private $router;
+
+    public function __construct($router)
+    {
+        $this->router = $router;
+    }
+
     public function getForm()
     {
         return new NodeContentType();
     }
 
-    public function resolveRouteName(Request $request)
+    public function resolveRouteName($formData = array())
     {
         //resolve from request and return the route name for node in nodes tree
 
-        return '/news';
+        return $formData['action'];
+    }
+
+
+    public function resolveControlRouteName($formData = array())
+    {
+        //resolve from request and return the route name for node in nodes tree
+
+        return $this->router->generate('cp_news');
     }
 }
