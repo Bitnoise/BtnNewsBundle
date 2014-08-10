28229db289fc829d4f173201c20468936d7e6830
Adam Misiorny <adam.misiorny@gmail.com>
2014-08-10 17:10:32 +0200

    slim up controller with new functions

--- Controller/NewsControlController.php
+++ Controller/NewsControlController.php
@@ -3,15 +3,17 @@
 namespace Btn\NewsBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
-use Btn\BaseBundle\Controller\AbstractController;
+use Btn\AdminBundle\Controller\AbstractControlController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Btn\AdminBundle\Annotation\EntityProvider;
 
 /**
  * @Route("/news")
+ * @EntityProvider("btn_news.provider.news")
  */
-class NewsControlController extends AbstractController
+class NewsControlController extends AbstractControlController
 {
     /**
      * @Route("/", name="btn_news_newscontrol_index")
@@ -19,15 +21,14 @@ class NewsControlController extends AbstractController
      */
     public function indexAction(Request $request)
     {
-        $perPage  = $this->container->getParameter('btn_admin.per_page');
-        $repo     = $this->get('btn_news.provider.news')->getRepository();
+        $repo     = $this->getEntityProvider()->getRepository();
         $entities = $repo->findAll();
 
         $paginator  = $this->get('knp_paginator');
         $pagination = $paginator->paginate(
             $entities,
             $request->query->getInt('page', 1),
-            $perPage
+            $this->getPerPage()
         );
 
         return array(
@@ -42,16 +43,13 @@ class NewsControlController extends AbstractController
      */
     public function createAction(Request $request)
     {
-        $entityProvider = $this->get('btn_news.provider.news');
-        $formHandler    = $this->get('btn_admin.form_handler');
-        $entity         = $entityProvider->create();
+        $entity = $this->getEntityProvider()->create();
 
         $form = $this->createForm('btn_news_form_news_control', $entity, array(
             'action' => $this->generateUrl('cp_news_create'),
         ));
 
-
-        if ($formHandler->handle($form)) {
+        if ($this->handleForm($form, $request)) {
             $this->setFlash('btn_admin.flash.created');
 
             return $this->redirect($this->generateUrl('cp_news_edit', array('id' => $entity->getId())));
@@ -70,15 +68,13 @@ class NewsControlController extends AbstractController
      */
     public function updateAction(Request $request, $id)
     {
-        $entityProvider = $this->get('btn_news.provider.news');
-        $formHandler    = $this->get('btn_admin.form_handler');
-        $entity         = $this->findEntityOr404($entityProvider->getClass(), $id);
+        $entity = $this->findEntityOr404($this->getEntityProvider()->getClass(), $id);
 
         $form = $this->createForm('btn_news_form_news_control', $entity, array(
             'action' => $this->generateUrl('cp_news_update', array('id' => $id)),
         ));
 
-        if ($formHandler->handle($form)) {
+        if ($this->handleForm($form, $request)) {
             $this->setFlash('btn_admin.flash.updated');
 
             return $this->redirect($this->generateUrl('cp_news_edit', array('id' => $id)));
@@ -97,7 +93,7 @@ class NewsControlController extends AbstractController
     {
         $this->validateCsrfTokenOrThrowException('cp_news_delete', $csrf_token);
 
-        $entityProvider = $this->get('btn_news.provider.news');
+        $entityProvider = $this->getEntityProvider();
         $entity         = $this->findEntityOr404($id, $entityProvider->getClass());
 
         $entityProvider->delete($entity);
