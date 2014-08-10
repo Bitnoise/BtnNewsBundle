d401d3ea9290f8f34b897fb0b0106f35d69a95ad
Adam Misiorny <adam.misiorny@gmail.com>
2014-08-10 09:27:50 +0200

    move logic to abstract repository

---
+++ Repository/AbstractNewsCategoryRepository.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Btn\NewsBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ *
+ */
+abstract class AbstractNewsCategoryRepository extends EntityRepository
+{
+    public function getSearchQuery($conditions)
+    {
+        $qb = $this->createQueryBuilder('nc')
+            ->select('nc')
+        ;
+        if (!empty($conditions) && is_array($conditions)) {
+            $expr = call_user_func_array(array($qb->expr(), 'andx'), $conditions);
+            $qb->where($expr);
+        }
+
+        return $qb;
+    }
+
+    /**
+     * @return \Btn\NewsBundle\NewsCategory[]
+     */
+    public function findAllVisible()
+    {
+        $query = $this
+            ->createQueryBuilder('nc')
+            ->andWhere('nc.visible = :visible')
+            ->orderBy('nc.position', 'ASC')
+            ->setParameter(':visible', 1)
+            ->getQuery()
+        ;
+
+        return $query->getResult();
+    }
+}
