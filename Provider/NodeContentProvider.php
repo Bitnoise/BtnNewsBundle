9c2a5b76f5b585134b0f7b8a9561959ccbd7e0e4
Adam Misiorny <adam.misiorny@gmail.com>
2014-08-12 23:40:13 +0200

    fix nodeContentProvider namespace

--- Provider/NodeContentProvider.php
+++ Provider/NodeContentProvider.php
@@ -2,7 +2,7 @@
 
 namespace Btn\NewsBundle\Provider;
 
-use Btn\NodesBundle\Service\NodeContentProviderInterface;
+use Btn\NodesBundle\Provider\NodeContentProviderInterface;
 use Btn\NewsBundle\Form\NodeContentType;
 use Btn\AdminBundle\Provider\EntityProviderInterface;
 
