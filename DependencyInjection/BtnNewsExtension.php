ded719d21e6bd60e1cdf910450652f99ab97ead0
Adam Misiorny <adam.misiorny@gmail.com>
2014-08-17 02:19:38 +0200

    use abstract extension

--- DependencyInjection/BtnNewsExtension.php
+++ DependencyInjection/BtnNewsExtension.php
@@ -2,36 +2,24 @@
 
 namespace Btn\NewsBundle\DependencyInjection;
 
+use Btn\BaseBundle\DependencyInjection\AbstractExtension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader;
 
 /**
- * This is the class that loads and manages your bundle configuration
  *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class BtnNewsExtension extends Extension
+class BtnNewsExtension extends AbstractExtension
 {
     /**
      * {@inheritDoc}
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
+        parent::load($configs, $container);
+
+        $config = $this->getProcessedConfig($container, $configs);
 
         $container->setParameter('btn_news.news.class', $config['news']['class']);
         $container->setParameter('btn_news.news_category.class', $config['news_category']['class']);
-
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('parameters.yml');
-        $loader->load('services.yml');
-        $loader->load('forms.yml');
-
-        if ($container->hasDefinition('btn_nodes.content_providers')) {
-            $loader->load('node-cp.yml');
-        }
     }
 }
