a38b1e64db1b2cdc63b382a55724b95e1c3e2475
Adam Misiorny <adam.misiorny@gmail.com>
2014-08-17 02:20:45 +0200

    fix content provider

--- Provider/NodeContentProvider.php
+++ Provider/NodeContentProvider.php
@@ -4,20 +4,27 @@ namespace Btn\NewsBundle\Provider;
 
 use Btn\NodesBundle\Provider\NodeContentProviderInterface;
 use Btn\NewsBundle\Form\NodeContentType;
-use Btn\AdminBundle\Provider\EntityProviderInterface;
+use Btn\BaseBundle\Provider\EntityProviderInterface;
 
 /**
  *
  */
 class NodeContentProvider implements NodeContentProviderInterface
 {
+    /** $var \Btn\BaseBundle\Provider\EntityProviderInterface $provider */
     protected $provider;
 
+    /**
+     *
+     */
     public function __construct(EntityProviderInterface $provider)
     {
         $this->provider = $provider;
     }
 
+    /**
+     *
+     */
     public function getForm()
     {
         $categories = $this->provider->getRepository()->findAll();
@@ -30,23 +37,43 @@ class NodeContentProvider implements NodeContentProviderInterface
         return new NodeContentType($data);
     }
 
+    /**
+     *
+     */
     public function resolveRoute($formData = array())
     {
         return 'app_news_category';
     }
 
+    /**
+     *
+     */
     public function resolveRouteParameters($formData = array())
     {
         return array('id' => $formData['category']);
     }
 
+    /**
+     *
+     */
     public function resolveControlRoute($formData = array())
     {
         return 'btn_news_newscontrol_index';
     }
 
+    /**
+     *
+     */
     public function resolveControlRouteParameters($formData = array())
     {
         return array('id' => $formData['category']);
+    }
+
+    /**
+     *
+     */
+    public function getName()
+    {
+        return 'btn_news.node_content_provider.name';
     }
 }
