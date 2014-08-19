9c13446980aa94e70f3b5914817260b543a6a12e
proszek <proszek@iMac-proszek.local>
2014-08-19 10:33:55 +0200

    Changed News entity - category can be blank now.

--- Entity/News.php
+++ Entity/News.php
@@ -4,7 +4,6 @@ namespace Btn\NewsBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Btn\NewsBundle\Model\AbstractNews;
-use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Table(name="btn_news", indexes={
@@ -16,7 +15,6 @@ use Symfony\Component\Validator\Constraints as Assert;
 class News extends AbstractNews
 {
     /**
-     * @Assert\NotBlank()
      * @ORM\ManyToOne(targetEntity="Btn\NewsBundle\Entity\NewsCategory", inversedBy="news")
      * @ORM\JoinColumn(name="category_id", referencedColumnName="id", onDelete="CASCADE")
      */
